e6116a6fb74ae9ee06ae46d54dbb33230b76f02f
Lucía Rolón <luciarolon00@gmail.com>
2024-06-30 20:19:14 -0300

    agregamos cards y temporizador

--- js/main.js
+++ js/main.js
@@ -21,28 +21,43 @@ const puntosUsuario = document.getElementById(".puntos-usuario");
 // Declaración de variables 
 let botonSgte;
 let puntos, preguntaActual, preguntasFinal;
+let temporizador = document.getElementsByClassName("timer")[0];
+let cuentaRegresiva, cuenta = 11;
 
 // Opciones aleatorias del array
-const generadorAleatorio = (array) => array[Math.floor(Math.random() * array.length)];
+const generadorValorAleatorio = (array) => array[Math.floor(Math.random() * array.length)];
 const mezclaAleatoria = (array) => array.sort(() => 0.5 - Math.random());
 
 // Empezar juego
 const iniciarJuego = () => {
-    puntosContainer.classList.add("hide")
-    juegoContainer.classList.remove("hide")
-    preguntasFinal = llenarPreguntas();
-    puntos = 0;
-    preguntaActual = 0;
-    llenarPreguntas(preguntasFinal[preguntaActual]);
+  puntosContainer.classList.add("hide")
+  juegoContainer.classList.remove("hide")
+  preguntasFinal = llenarPreguntas();
+  puntos = 0;
+  preguntaActual = 0;
+  // Que salga la primera pregunta
+  generadorTarjeta(preguntasFinal[preguntaActual]);
+};
+
+// TEMPORIZADOR
+const mostrarTemporizador = () => {
+  cuentaRegresiva = setInterval(() => {
+    cuenta -= 1;
+    temporizador.innerHTML = `<span>Tiempo restante: </span>${cuenta}s`;
+    if (cuenta == 0) {
+      clearInterval(cuentaRegresiva);
+      siguientePregunta();
+    }
+  }, 1000);
 };
 
 // Crear opciones
-const llenarPreguntas = (opcionCorrecta) => {
-    let arr = [];
-    arr.push(opcionCorrecta);
-    let cantidadOpciones = 1;
-    while (cantidadOpciones < 4) {
-    let valorAleatorio = generadorAleatorio(opcionesArray);
+const llenarOpciones = () => {
+  let arr = [];
+  arr.push(opcionCorrecta);
+  let cantidadOpciones = 1;
+  while (cantidadOpciones < 4) {
+    let valorAleatorio = generadorValorAleatorio(opcionesArray);
     if (!arr.includes(valorAleatorio)) {
       arr.push(valorAleatorio);
       cantidadOpciones += 1;
@@ -50,3 +65,90 @@ const llenarPreguntas = (opcionCorrecta) => {
   }
   return arr;
 }
+
+// Preguntas Aleatorias
+const llenarPreguntas = () => {
+  let cantidadPreguntas = 0;
+  let objetosElegidos = [];
+  let lotePreguntas = [];
+  while (cantidadPreguntas < 5) {
+    let valorAleatorio = generadorValorAleatorio(preguntas);
+    let indice = preguntas.indexOf(valorAleatorio);
+    if (!objetosElegidos.includes(indice)) {
+      lotePreguntas.push(valorAleatorio);
+      objetosElegidos.push(indice);
+      cantidadPreguntas += 1;
+    }
+  }
+  return lotePreguntas;
+};
+
+//Comprobar respuesta seleccionada
+const comprobar = (e) => {
+  let solucionUsuario = e.target.innerText;
+  let opciones = document.querySelectorAll(".option");
+  if (solucionUsuario === preguntasFinal[preguntaActual].opcionCorrecta) {
+    e.target.classList.add("Correcta");
+    puntaje++;
+  } else {
+    e.target.classList.add("Incorrecta");
+    opciones.forEach((element) => {
+      if (element.innerText == preguntasFinal[preguntaActual].opcion_correcta) {
+        element.classList.add("Correcta");
+      }
+    });
+  }
+
+  clearInterval(cuentaRegresiva);
+  //Deshabilitar todas las opciones
+  opciones.forEach((element) => {
+    element.disabled = true;
+  });
+};
+
+//Siguiente pregunta
+const siguientePregunta = (e) => {
+  preguntaActual += 1;
+  if (preguntaActual == preguntasFinal.length) {
+    contenedorJuego.classList.add("hide");
+    contenedorPuntaje.classList.remove("hide");
+    botonInicio.innerText = `Reiniciar`;
+    puntajeUsuario.innerHTML =
+      "Tu puntaje es " + puntos + " de " + preguntaActual;
+    clearInterval(cuentaRegresiva);
+  } else {
+    generadorTarjeta(preguntasFinal[preguntaActual]);
+  }
+};
+
+// TARJETA
+const generadorTarjeta = (objetoTarjeta) => {
+  const { imagen, opcionCorrecta } = objetoTarjeta;
+  let opciones = mezclaAleatoria(llenarOpciones(opcionCorrecta));
+  container.innerHTML = `<div class="quiz">
+  <p class="num">
+  ${preguntaActual + 1}/5
+  </p>
+  <div class="preguntas">
+    <img class="pokemon-imagen" src="${imagen}"/>
+  </div>
+  
+  <div class="opciones">
+    <button class="opcion" onclick="comprobar(event)">${opciones[0]}
+    </button>
+    <button class="opcion" onclick="comprobar(event)">${opciones[1]}
+    </button>
+    <button class="opcion" onclick="comprobar(event)">${opciones[2]}
+    </button>
+    <button class="opcion" onclick="comprobar(event)">${opciones[3]}
+    </button>
+  </div>
+ 
+  <div class="div-btn-sgte">
+    <button class="btnSgte" onclick="siguientePregunta(evento)">Siguiente</button>
+  </div>
+
+  </div>`;
+}
+
+startButton.addEventListener("click", iniciarJuego);
